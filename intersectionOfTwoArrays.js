911cf40074b3e0240004ced5d8cb89c9a769c0ac
Jonnatan Ortiz <jonnatano94@gmail.com>
2019-02-19 14:48:01 -0800

    Work on intersectionOfTwoArrays leetcode problem

---
+++ intersectionOfTwoArrays.js
@@ -0,0 +1,27 @@
+/*Given two arrays, write a function to compute their intersection.
+
+    Example 1:
+
+Input: nums1 = [1, 2, 2, 1], nums2 = [2, 2]
+Output: [2]
+Example 2:
+
+Input: nums1 = [4, 9, 5], nums2 = [9, 4, 9, 8, 4]
+Output: [9, 4]
+Note:
+
+Each element in the result must be unique.
+The result can be in any order.
+*/
+
+var intersection = function (nums1, nums2) {
+    let result = [];
+    for (let i = 0; i < nums1.length; i++) {
+        if (nums2.includes(nums1[i]) && !result.includes(nums1[i])) {
+            result.push(nums1[i]);
+        }
+    }
+    return result;
+
+};
+
