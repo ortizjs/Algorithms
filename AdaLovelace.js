444ed2a0e8fb114a706cd3be96d9484f5d97d6c4
Jonnatan Ortiz <jonnatano94@gmail.com>
2019-03-07 21:30:27 -0800

    Add Ada Lovelace coding challenge question 2 (wepay)

---
+++ AdaLovelace.js
@@ -0,0 +1,33 @@
+// function ada(year){
+//   let tuesday = 0;
+//   let sum = 0;
+//   while(tuesday <= 2) {
+//     if (num < 2) {
+//       sum += ((6 + 2) - num);
+//       num = 2;
+//       tuesday += 1;
+//     } else if (num > 2) {
+//       sum += (2 - num);
+//       num = 2
+//       tuesday += 1
+//     }
+//   }
+//   return sum;
+// }
+
+let ada = function (year) {
+    let weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let date = new Date("October 1, " + year).getUTCDay();
+    let day = 0;
+    let count = 0;
+    while (count < 2) {
+        day += 1;
+        if (weekdays[(date % 7)] === "Tuesday") {
+            count += 1
+        }
+        date += 1;
+    }
+    console.log(day)
+    // return day;
+}
+ada(2019);
