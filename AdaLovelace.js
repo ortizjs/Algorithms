1f4f82919c1026fb4d21adbdf4cf8155dff0c0f2
Jonnatan Ortiz <jonnatano94@gmail.com>
2019-03-07 21:31:43 -0800

    Add new solutionto ada lovelace problem

--- AdaLovelace.js
+++ AdaLovelace.js
@@ -15,19 +15,35 @@
 //   return sum;
 // }
 
-let ada = function (year) {
-    let weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+// let ada = function (year) {
+//     let weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+//     let date = new Date("October 1, " + year).getUTCDay();
+//     let day = 0;
+//     let count = 0;
+//     while (count < 2) {
+//         day += 1;
+//         if (weekdays[(date % 7)] === "Tuesday") {
+//             count += 1
+//         }
+//         date += 1;
+//     }
+//     console.log(day)
+//     // return day;
+// }
+const ada = function (year) {
     let date = new Date("October 1, " + year).getUTCDay();
+
     let day = 0;
     let count = 0;
     while (count < 2) {
         day += 1;
-        if (weekdays[(date % 7)] === "Tuesday") {
-            count += 1
+        if ((date % 7) === 2) {
+            count += 1;
+
         }
         date += 1;
+
     }
-    console.log(day)
-    // return day;
-}
+    console.log(day + " result");
+};
 ada(2019);
