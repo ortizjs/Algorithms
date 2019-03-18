1936ce4c0388cb27e343a08cf3ac618f3844faf8
Jonnatan Ortiz <jonnatano94@gmail.com>
2019-03-17 21:58:36 -0700

    Add permutations problem

--- permutations.js
+++ permutations.js
@@ -1,5 +1,45 @@
-let permutations = function(array){
+// let permutations = function(array){
 
-};
+// };
 
-permutations
+// permutations
+
+const helper = (strArr, char) => {
+    // strArr = ["ab", "ba"]
+    // char = "c"
+    // str in each spot on arr
+
+    // return [
+    // "abc", "acb", "cab",
+    // "bac", "bca", "cba"
+    // ]
+
+    let returnArr = [];
+
+    for (let i = 0; i < strArr.length; i++) {
+        const currentBaseStr = strArr[i];
+
+        const baseStrArr = currentBaseStr.split("");
+        for (let j = 0; j <= baseStrArr.length; j++) {
+            const baseStrArrCopy = [...baseStrArr];
+            baseStrArrCopy.splice(j, 0, char);
+            returnArr.push(baseStrArrCopy.join(""));
+        }
+    }
+
+    return returnArr;
+}
+
+function perms(str) {
+    const chars = str.split("");
+
+    let returnArr = [""];
+
+    chars.forEach(char => {
+        returnArr = helper(returnArr, char)
+    });
+
+    return returnArr;
+}
+
+console.log(perms("abcdefg"));
