51a7ce90562cb9e077de16496c5d5c2149d8f193
Jonnatan Ortiz <jonnatano94@gmail.com>
2019-03-17 18:28:35 -0700

    Sunday code

--- minChange.js
+++ minChange.js
@@ -26,3 +26,4 @@ var minChange = function(coins, amount) {
 // minChange([1,5,10, 25], 15) // 2 because 5 + 10 = 25
 minChange([1, 5, 10, 25], 100) // 4 because 25 + 25 + 25 + 25= 100
 // minChange([20,20,20,20], 11) // 4 because 25 + 25 + 25 + 25= 100
+//
